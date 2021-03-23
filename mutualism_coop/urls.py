f3e81ab661ce748a123c237d4c926e4de1fed0c9
dtluna <dtluna@riseup.net>
2021-03-24 02:14:29 +0300

    Configure i18n

--- mutualism_coop/urls.py
+++ mutualism_coop/urls.py
@@ -1,5 +1,6 @@
 import debug_toolbar
 
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
@@ -14,7 +15,6 @@ urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
-    path("search/", search_views.search, name="search"),
 ]
 
 
@@ -27,7 +27,8 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
 
-urlpatterns = urlpatterns + [
+urlpatterns += i18n_patterns(
+    path("search/", search_views.search, name="search"),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
@@ -35,4 +36,4 @@ urlpatterns = urlpatterns + [
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    path("pages/", include(wagtail_urls)),
-]
+)
