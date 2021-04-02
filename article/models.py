0e6a8eb65163ef0308d9f5f5e4e223f9bcd5d9c1
dtluna <dtluna@riseup.net>
2021-04-03 00:00:14 +0300

    Add author field to ArticlePage

--- article/models.py
+++ article/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, TranslatableMixin
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+
+@register_snippet
+class Author(TranslatableMixin, models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class ArticlePage(Page):
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, blank=False, null=True,
+    )
     content = RichTextField()
     image = models.ForeignKey(
         "wagtailimages.Image",
@@ -19,4 +32,5 @@ class ArticlePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("content", classname="full"),
         ImageChooserPanel("image"),
+        SnippetChooserPanel("author"),
     ]
